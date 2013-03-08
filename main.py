6868953aff2e375c3f22d308f0f09cc7192ea2b4
Lento Manickathan <lento.manickathan@gmail.com>
2013-03-08 16:09:23 +0100

    Evaluating error between analytic solution

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ ion() # Interactive on
 
 # Control Parameters
 windspeed = [[1.], [0.]] # x-dir and y-dir respectively
-n_panels  = 100.
+n_panels  = 5
 
 #==============================================================================
 # Initialization of bodies
@@ -51,24 +51,24 @@ airfoilB = body(name        = 'airfoilB',
                 pivot_point = [0.25, 0.])
            
 tower    = body(name        = 'tower',
-                shape       = ('cylinder', 100),
+                shape       = ('cylinder', n_panels),
                 chord       = 1,
                 local_pitch = 0.,
                 pivot_point = [0.5, 0.])
-
+'''
 # Creating multi-body
-#windturbine = multiBody(dict(body         = tower,
-#                             location     = [0.,0.],
-#                             global_pitch = 0.),
-#                             
-#                        dict(body         = airfoilA,
-#                             location     = [0.,0.],
-#                             global_pitch = 0.),  
-#                        
-#                        dict(body         = airfoilB,
-#                             location     = [0.,-2.], 
-#                             global_pitch = 180.))
+windturbine = multiBody(dict(body         = tower,
+                             location     = [0.,0.],
+                             global_pitch = 0.),
+                             
+                        dict(body         = airfoilA,
+                             location     = [0.,2.],
+                             global_pitch = 0.),  
                         
+                        dict(body         = airfoilB,
+                             location     = [0.,-2.], 
+                             global_pitch = 180.))
+'''                      
 cylinder_testCase = multiBody(dict(body         = tower,
                                    location     = [0., 0.],
                                    global_pitch = 0.))
@@ -80,7 +80,7 @@ cylinder_testCase = multiBody(dict(body         = tower,
 #vort         = vortex([-1.],[0.],[1.])
 
 # Plotting: Mesh grid
-x,y = meshgrid(linspace(-1,1,200),linspace(-1,1,200))
+x,y = meshgrid(linspace(-1,1,50),linspace(-1,1,50))
 mesh = array([concatenate(x),concatenate(y)])
  
  
@@ -92,12 +92,24 @@ mesh = array([concatenate(x),concatenate(y)])
 cylinder_testCase.vortexPanel_solve(evaluationPoints  = mesh,
                                     vortexPoints      = None,
                                     freestream        = windspeed)
+                                
 
 # Total Velocity: sum of source induction and windspeed
 V_tot = cylinder_testCase.Vinduced + windspeed
 
+'''
+# Solve the panelMethod
+windturbine.vortexPanel_solve(evaluationPoints        = mesh,
+                                    vortexPoints      = None,
+                                    freestream        = windspeed)
+                  
+# Total Velocity: sum of source induction and windspeed
+V_tot = windturbine.Vinduced + windspeed
+'''
+
+
 Vres = (V_tot[0]**2 + V_tot[1]**2)**0.5
-Vres[Vres>2] = nan
+Vres[Vres>2.8] = nan
 
 #==============================================================================
 # TEST PLOTTING
@@ -106,8 +118,30 @@ Vres[Vres>2] = nan
 # Plotting 
 figure()
 cylinder_testCase.plot('geometry')
-axis('scaled')
+#contourf(x,y,reshape(Vres,shape(x)))
+quiver(mesh[0],mesh[1], V_tot[0], V_tot[1],Vres)
 axis([-1,1,-1,1])
-contourf(x,y,reshape(Vres,shape(x)))
-#quiver(cylinder_testCase.collocationPoint[0],cylinder_testCase.collocationPoint[1],V_tot[0],V_tot[1],Vres)
+axis('scaled')
 colorbar()
+
+phi = linspace(-180+(180/n_panels),180+(180/n_panels),n_panels)
+vs  = 2*np.sin(np.deg2rad(phi))*windspeed[0]
+
+figure()
+plot(0.5*cos(np.deg2rad(phi)),vs)
+plot(cylinder_testCase.collocationPoint[0],cylinder_testCase.vortex_gamma,'.')
+#plot()
+#axis('equal')
+axis([-1,1,0,2])
+grid('on')
+
+#==============================================================================
+# Error
+#==============================================================================
+err = average((abs(vs-cylinder_testCase.vortex_gamma)*abs(cylinder_testCase.geometry[0,1:]-cylinder_testCase.geometry[0,:-1]))/(sum(abs(cylinder_testCase.geometry[0,1:]-cylinder_testCase.geometry[0,:-1]))))
+print err
+
+figure()
+plot(array([4,10,20,30,40,50,60,70,80,90,100,200,500,1000]),
+     array([0.46635927869,0.2737,0.1486,0.1010,0.0765,0.0615,0.05148,0.04425,0.038759,0.034526,0.03111,0.015633557165,0.00627131268198,0.0031]),'.-')
+grid('on')
